5d52f7706202959e66ba42562402c816362a8bb5
lanaa-dev <rivaldiahmad555@gmail.com>
2021-06-05 22:52:23 +0800

    deklarasi variabel dan tipe data

---
+++ Modul01/DataTypes.java
@@ -0,0 +1,14 @@
+public class DataTypes{
+ public static void main(String[] args){
+
+  String namaDepan = "Maulana";
+  String namaTengah = "Iskandar";
+  String namaBelakang = "bii";
+  int usia = 20;
+  int targetTahunKuliah = 4;
+  double ipk = 3.89764512;
+  char nilaiAbjad = 'A';
+  boolean tampan = true;
+
+ }
+}
